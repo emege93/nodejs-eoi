79d7f054577a8e9152eac95c41649f4f2752fc7c
emege93 <emege93@gmail.com>
2020-09-16 10:42:19 +0200

    fix

--- router.js
+++ router.js
@@ -1,4 +1,3 @@
-const { response } = require("express");
 const express = require("express");
 const router = express.Router();
 
@@ -12,11 +11,51 @@ router.get("/", (request, response) => {
   response.render("index");
 });
 
+router.get("/admin", (request, response) => {
+  const session = request.session;
+
+  if (session.email) {
+    response.render("admin", { email: session.email });
+  }
+
+  return response.redirect("/login");
+});
+
+router.get("/login", (request, response) => {
+  const session = request.session;
+
+  if (session.email) {
+    return response.redirect("/admin");
+  }
+
+  response.render("login");
+});
+
+router.post("/login", (request, response) => {
+  request.session.email = request.body.email;
+  return response.redirect("/admin");
+});
+
+router.get("/logout", (request, response) => {
+  request.session.destroy((err) => {
+    if (err) {
+      console.log(err);
+    }
+    response.redirect("/");
+  });
+});
+
 // POST CRUD API
 const POST_ENDPOINT = "/api/v1/posts";
 
+router.use((request, response, next) => {
+  console.log(`Router Time: ${Date.now()}`);
+  next();
+});
+
 router.post(POST_ENDPOINT, (request, response) => {
   const post = request.body;
+  // TODO Save it somewhere
   response.send("POST");
 });
 
@@ -34,40 +73,6 @@ router.put(`${POST_ENDPOINT}/:id`, (request, response) => {
 
 router.delete(`${POST_ENDPOINT}/:id`, (request, response) => {
   response.send(`DELETE ${request.params.id}`);
-});
-
-router.get("/admin", (request, response) => {
-  const session = request.session;
-
-  if (session.email) {
-    response.write(`<h1>Hola ${session.email}</h1><br>`);
-    response.end('<a href="/logout">Logout</a>');
-  } else {
-    return response.redirect("/login");
-  }
-});
-
-router.get("/login", (request, response) => {
-  const session = request.session;
-
-  if (session.email) {
-    return response.redirect("/admin");
-  }
-
-  response.render("login");
-});
-router.get("/login", (request, response) => {
-  const session = request.session;
-  session.email = request.body.email;
-  response.end("correcto");
-});
-router.get("/logout", (request, response) => {
-  request.session.destroy((err) => {
-    if (err) {
-      console.log(err);
-    }
-    response.redirect("/");
-  });
 });
 
 module.exports = router;
